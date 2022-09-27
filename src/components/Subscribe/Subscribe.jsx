5e88486e9507ac200a7b1882427f75055a249383
Heytemisan <temyicy@gmail.com>
2022-09-27 14:10:06 +0100

    fix: style cleanup

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -5,38 +5,33 @@ import "./Subscribe.css";
 function Subscribe() {
   return (
     <section className="subscribe">
-        <div className="subscribe-details">
-          <div className="subscribe-header">
-            <img src={PBlogo} alt="planetbase logo" />
+      <div className="subscribe-details">
+        <img src={PBlogo} alt="planetbase logo" className="subscribe-img" />
+        <h4>
+          Moving your events online really opens up the esteem of supporting it.
+        </h4>
+        <p>Become a subscriber and become the first to receive Information.</p>
+      </div>
+      <div>
+        <div class="parent-wrapper">
+          <span></span>
+          <div class="subscribe-wrapper">
+            <h4>SUBSCRIBE TO OUR NEWSLETTER</h4>
+            <input
+              type="email"
+              name="email"
+              className="subscribe-input"
+              placeholder="Enter Your Email Address"
+            />
+            <button className="subscribe-btn">SUBMIT</button>
           </div>
-          <h4>
-            Moving your events online really opens up the esteem of supporting
-            it
-          </h4>
-          <p>
-            Become a subscriber and become the first to receive Information.
-          </p>
         </div>
-        <div>
-          <div class="parent-wrapper">
-            <span></span>
-            <div class="subscribe-wrapper">
-              <h4>SUBSCRIBE TO OUR NEWSLETTER</h4>
-              <input
-                type="email"
-                name="email"
-                className="subscribe-input"
-                placeholder="Enter Your Email Address"
-              />
-              <div className="submit-btn">SUBMIT</div>
-            </div>
-          </div>
 
-          <summary className="policy">
-            By subscribing to our news letter, you have agreed to our Terms of
-            Service and Privacy Policy.
-          </summary>
-        </div>
+        <summary className="policy">
+          By subscribing to our news letter, you have agreed to our Terms of
+          Service and Privacy Policy.
+        </summary>
+      </div>
     </section>
   );
 }
